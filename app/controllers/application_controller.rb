b544eaf5f2e44a2b6ffee233f9e2817bfa19818e
John Purnell <john.purnell@gmail.com>
2019-10-22 17:10:22 -0600

    collection based partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,10 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
   
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 
 end
